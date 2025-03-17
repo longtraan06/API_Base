7a17e377bbf4e47d7ebd2de4333ca7c84dfeeb0d
Long traan <traanngoclong10a4@gmail.com>
2025-03-17 16:00:18 +0700

    lmao

--- convert_json_to_list.py
+++ convert_json_to_list.py
@@ -2,17 +2,13 @@ from fastapi import UploadFile
 import json
 import aiofiles
 
-async def convert_jsonl_to_dict(file: UploadFile):
-    data_list = []
+async def convert_json_to_dict(file: UploadFile):
+
     async with aiofiles.open(file.filename, 'wb') as out_file:
         content = await file.read()
         await out_file.write(content)
 
     async with aiofiles.open(file.filename, 'r', encoding='utf-8') as f:
-        async for line in f:
-            if not line.strip():
-                continue
-            record = json.loads(line)
-            data_list.append(record)
+        data = dict(f)
     
-    return data_list
+    return data
