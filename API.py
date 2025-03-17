0a52deb054b0a540a05fc9b9261e384c5b0970d8
Long traan <traanngoclong10a4@gmail.com>
2025-03-17 15:59:26 +0700

    lmao

--- API.py
+++ API.py
@@ -1,28 +1,28 @@
 from fastapi import FastAPI, UploadFile, File
 import json
 import aiofiles
-from fastapi.responses import StreamingResponse
-import io
-from convert_json_to_list import *
+from fastapi.responses import JSONResponse
 
 
 app = FastAPI()
 
 # Endpoint nhận file JSONL, xử lý và trả về JSONL
-@app.post("/process-jsonl/")
-async def process_jsonl(file: UploadFile = File(...)):
-    # Gọi hàm convert to list dict
-    data_list = await convert_jsonl_to_dict(file) 
+@app.post("/process-json/")
+async def process_json(file: UploadFile = File(...)):
+    # Đọc file JSON
+    async with aiofiles.open(file.filename, "r", encoding="utf-8") as f:
+        content = await f.read()
+    
+    data_dict = json.loads(content) # convert file json sang dict
 
     #####################
 
     # trong đây là nơi đặt model sử lý data list
-    # model này sẽ trả về một list_dict
+    # model này sẽ trả về một dict
+    processed_data = [{"processed": True, **data_dict}] # ví dụ đây là file list dict kết qủa sau khi sử lý
+
     #####################
     
-    processed_data = [{"processed": True, **record} for record in data_list] # ví dụ đây là file list dict kết qủa sau khi sử lý
     
     # Chuyển kết quả thành JSONL để trả về
-    jsonl_content = "\n".join(json.dumps(record, ensure_ascii=False) for record in processed_data) # thay processed_data bằng list_dict kết quả
-    return StreamingResponse(io.StringIO(jsonl_content), media_type="application/jsonl", headers={"Content-Disposition": "attachment; filename=output.jsonl"})
-
+    return JSONResponse(content=processed_data, media_type="application/json") # thay processed_data = data từ model
